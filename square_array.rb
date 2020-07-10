28d336033154f0331ca4a9e1d0c383a7684d6546
Ebo Lee <yiboli.coding@gmail.com>
2020-07-10 03:59:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,9 +4,14 @@
 def square_array(numbers)
   arr = []
   numbers.each do |number| 
-    arr << #{number**2}
+    arr << number**2
     arr
   end 
 end
 
+def square_array(numbers)
+  arr = []
+  numbers.each { |number| arr << number ** 2 }
+  arr
+end
 
