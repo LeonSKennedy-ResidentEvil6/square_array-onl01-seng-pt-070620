c7e293e050299ca3682eb7bcfddb527e15cfa983
Ebo Lee <yiboli.coding@gmail.com>
2020-07-10 04:09:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
-
 def square_array(numbers)
-  sqaured = #{numbers.collect { |number| number ** 2 }}
-  return sqaured
-end 
-
+  arr = []
+  numbers.each { |number| arr << number ** 2 }
+  arr
+end
